b7f5684a5f34d5192dddc343466c113f69468fde
Mmp <mmpneo@gmail.com>
2023-02-06 23:47:30 +0300

    up

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
