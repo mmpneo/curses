63ddbd2ce00938314f6eb3225ba87e18b8499b81
Mmp <mmpneo@gmail.com>
2023-02-23 15:42:36 +0300

    feat: loading screen for client

---
+++ src/client/ui/view_loading.tsx
@@ -0,0 +1,17 @@
+import Logo from "@/server/ui/logo";
+import style from "./loading.module.css";
+
+export default function ClientLoadingView() {
+  return (
+    <div data-theme="curses" className={style.screen}>
+      <div className="p-6 py-4 bg-black/30 animate-pulse leading-none rounded-box text-2xl flex flex-col gap-2 text-secondary">
+        <Logo />{" "}
+        <span className="flex items-center text-lg gap-2">
+          <img height="20px" width={24} src="/images/ui-client-loading.gif"/>
+          {/* <span className={style.spinner}></span> */}
+          connecting...
+        </span>
+      </div>
+    </div>
+  );
+}
