68b12dcc0f7f08b0370c8e53ed52b760ef16afc5
Mmp <mmpneo@gmail.com>
2023-02-23 12:37:33 +0300

    chore: refactor

--- src/server/ui/recording-alerts.tsx
+++ src/server/ui/recording-alerts.tsx
@@ -5,15 +5,15 @@ import { SiDiscord, SiTwitch } from "react-icons/si";
 import { useSnapshot } from "valtio";
 import Tooltip from "./dropdown/Tooltip";
 
-const StatusButton: FC<PropsWithChildren<{key: string, tooltip: string, onClick: () => void}>> = ({key, tooltip, onClick, children}) => {
+const StatusButton: FC<PropsWithChildren<{animKey: string, tooltip: string, onClick: () => void}>> = ({animKey, tooltip, onClick, children}) => {
   return <motion.div
-  key={key}
+  key={animKey}
   initial={{ opacity: 0, scale:0 }}
   exit={{ opacity: 0, scale: 0 }}
   animate={{ opacity: 1, scale: 1 }}
   transition={{ ease: "anticipate", duration: 0.5 }}
 >
-  <Tooltip content={tooltip} body="Click to stop">
+  <Tooltip content={tooltip} body={animKey}>
     <button onClick={onClick} className="btn btn-sm btn-warning btn-circle">
       {children}
     </button>
@@ -22,14 +22,14 @@ const StatusButton: FC<PropsWithChildren<{key: string, tooltip: string, onClick:
 }
 
 const RecordingAlerts: FC = () => {
-  const { postEnable: discordPostEnable } = useSnapshot(
+  const { postEnable: discordPostEnable, postWithTwitchLive: discordPostWithTwitchLive } = useSnapshot(
     window.ApiServer.state.services.discord.data
   );
   const { chatPostEnable, chatEnable, chatPostLive } = useSnapshot(
     window.ApiServer.state.services.twitch.data
   );
-  const { chatStatus: twitchChatStatus, liveStatus: twitchLiveStatus } =
-    useSnapshot(window.ApiServer.twitch.state);
+  const { liveStatus: twitchLiveStatus } = useSnapshot(window.ApiServer.twitch.state);
+  const { connection: twitchChatStatus } = useSnapshot(window.ApiServer.twitch.chat.state);
 
   const handleStopTwitch = () => {
     window.ApiServer.patchService("twitch", state => state.data.chatPostEnable = false)
@@ -42,11 +42,15 @@ const RecordingAlerts: FC = () => {
     chatPostEnable &&
     twitchChatStatus === ServiceNetworkState.connected &&
     (chatPostLive ? twitchLiveStatus === ServiceNetworkState.connected : true);
+
+
+    
+    const showDiscord = discordPostEnable && (discordPostWithTwitchLive ? twitchLiveStatus === ServiceNetworkState.connected : true)
   return (
     <div className="flex space-x-2">
       <AnimatePresence>
-        {showTwitch && <StatusButton tooltip="Twitch Chat integration" key="twitch" onClick={handleStopTwitch }><SiTwitch size={20} /></StatusButton>}
-        {discordPostEnable && <StatusButton tooltip="Discord integration" key="discord" onClick={handleStopDiscord }><SiDiscord size={20} /></StatusButton>}
+        {showTwitch && <StatusButton key="twitch" tooltip="Twitch Chat integration" animKey="twitch" onClick={handleStopTwitch }><SiTwitch size={20} /></StatusButton>}
+        {showDiscord && <StatusButton key="discord" tooltip="Discord integration" animKey="discord" onClick={handleStopDiscord }><SiDiscord size={20} /></StatusButton>}
       </AnimatePresence>
     </div>
   );
