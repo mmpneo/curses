ba742a3cf7e7c092be5a63d971b6869d2b8d4c1c
Mmp <mmpneo@gmail.com>
2023-04-18 08:12:42 +0300

    refactor: add error support

--- src/server/ui/service-button.tsx
+++ src/server/ui/service-button.tsx
@@ -1,15 +1,21 @@
 import { FC }                  from "react";
 import { ServiceNetworkState } from "../../types";
 
-const ServiceButton: FC<{
+type Props = {
   status: ServiceNetworkState,
   startLabel?: string,
   stopLabel?: string,
+  errorLabel?: string,
+  showError?: boolean
   onStart: () => void,
-  onStop: () => void}> = ({status, startLabel = "Start", stopLabel = "Stop", onStart, onStop}) => {
+  onStop: () => void,
+  onError?: () => void,
+}
+
+const ServiceButton: FC<Props> = ({status, showError = false, errorLabel = "Error. Try again!", startLabel = "Start", stopLabel = "Stop", onStart, onStop, onError}) => {
   return <>
     {status === ServiceNetworkState.disconnected && <button className="btn btn-sm btn-primary" onClick={onStart}>{startLabel}</button>}
-    {/* {status === ServiceNetworkState.error && <button className="btn btn-sm btn-primary" onClick={onStart}>Start</button>} */}
+    {showError && status === ServiceNetworkState.error && <button className="btn btn-sm btn-error" onClick={() => onError?.()}>{errorLabel}</button>}
 
     {status === ServiceNetworkState.connecting && <button className="btn btn-sm btn-primary loading">Connecting</button>}
     {status === ServiceNetworkState.connected && <button className="btn btn-sm btn-primary" onClick={onStop}>{stopLabel}</button>}
