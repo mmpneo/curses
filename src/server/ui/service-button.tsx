e0d0cc771ce3b727a2296109c604da0e0e304ddc
Mmp <mmpneo@gmail.com>
2023-04-18 17:01:23 +0300

    feat: allow click on loading

--- src/server/ui/service-button.tsx
+++ src/server/ui/service-button.tsx
@@ -9,15 +9,16 @@ type Props = {
   showError?: boolean
   onStart: () => void,
   onStop: () => void,
+  onPending?: () => void,
   onError?: () => void,
 }
 
-const ServiceButton: FC<Props> = ({status, showError = false, errorLabel = "Error. Try again!", startLabel = "Start", stopLabel = "Stop", onStart, onStop, onError}) => {
+const ServiceButton: FC<Props> = ({status, showError = false, errorLabel = "Error. Try again!", startLabel = "Start", stopLabel = "Stop", onStart, onStop, onPending, onError}) => {
   return <>
     {status === ServiceNetworkState.disconnected && <button className="btn btn-sm btn-primary" onClick={onStart}>{startLabel}</button>}
     {showError && status === ServiceNetworkState.error && <button className="btn btn-sm btn-error" onClick={() => onError?.()}>{errorLabel}</button>}
 
-    {status === ServiceNetworkState.connecting && <button className="btn btn-sm btn-primary loading">Connecting</button>}
+    {status === ServiceNetworkState.connecting && <button className="btn btn-sm btn-primary" onClick={() => onPending?.()}>Connecting</button>}
     {status === ServiceNetworkState.connected && <button className="btn btn-sm btn-primary" onClick={onStop}>{stopLabel}</button>}
   </>
 }
