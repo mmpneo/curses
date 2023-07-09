6fe5d5e99adecef56d567f606f336c1ee92af6f9
Mmp <mmpneo@gmail.com>
2023-07-09 14:43:46 +0300

    feat(scenes): add scenes

--- src/client/services/scenes/index.ts
+++ src/client/services/scenes/index.ts
@@ -1,31 +1,79 @@
 import { IServiceInterface } from "@/types";
 import { nanoid }            from "nanoid";
+import { proxy } from "valtio";
+import { SceneStateSchema } from "./schema";
 
 class Service_Scenes implements IServiceInterface {
-  init(): void {}
+  state = proxy({
+    activeScene: 'main'
+  });
 
-  addScene(name: string) {
+  get scenes() {
+    return window.ApiClient.document.fileBinder.get().scenes;
+  }
+
+  init(): void {
+    if (window.Config.isClient() && window.Config.clientInitialState?.scene) {
+      this.state.activeScene = window.Config.clientInitialState?.scene ?? "main";
+    }
+    else 
+      this.state.activeScene = window.ApiClient.document.fileBinder.get().activeScene;
+    window.ApiShared.pubsub.subscribe("scenes:change", e => {
+      if (typeof e === "string" && e in this.scenes) {
+        this.state.activeScene = e;
+      }
+    });
+  }
+
+  addScene() {
     window.ApiClient.document.patch((state) => {
-      const id = nanoid();
-      state.scenes[id] = {
-        id,
-        name,
-        bindOBS: false,
-        bindOBSName: "Some OBS scene",
-      };
+      let id = nanoid();
+      while (id in state.scenes)
+        id = nanoid();
+      state.scenes[id] = SceneStateSchema.parse({id});
+    });
+  }
+
+  duplicateScene(ogId: string) {
+    window.ApiClient.document.patch((state) => {
+      if (!(ogId in state.scenes))
+        return;
+
+      let id = nanoid();
+      while (id in state.scenes)
+        id = nanoid();
+
+      // clone scene
+      state.scenes[id] = {...state.scenes[ogId]};
+      state.scenes[id].id = id;
+      state.scenes[id].name += ' (Copy)';
+
+      // clone scene data in elements
+      for (const elId in state.elements) {
+        if (ogId in state.elements[elId].scenes) {
+          state.elements[elId].scenes[id] = {...state.elements[elId].scenes[ogId]};
+        }
+      }
     });
   }
 
   setActive(id: string): void {
-    window.ApiClient.document.patch((state) => {
-      if (state.scenes[id]) state.activeScene = id;
-    });
+    window.ApiShared.pubsub.publish("scenes:change", id);
   }
 
   deleteScene(id: string) {
+    if (this.state.activeScene === id)
+      this.state.activeScene = "main";
+
     window.ApiClient.document.patch((state) => {
-      if (state.scenes[id]) delete state.scenes[id];
-      // delete element scene instances
+      console.log(JSON.parse(JSON.stringify(state)))
+      if (id in state.scenes)
+        delete state.scenes[id];
+      for (const elementKey in state.elements) {
+        if (id in state.elements[elementKey].scenes) {
+          delete state.elements[elementKey].scenes[id];
+        }
+      }
     });
   }
 }
